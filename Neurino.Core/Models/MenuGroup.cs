33228773e7c16986cbf3fc2ce91d9118dc33dcc3
Sławomir Dobroś <slawektychy@gmail.com>
2025-04-07 14:15:47 +0200

    zmiany

---
+++ Neurino.Core/Models/MenuGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neurino.Core.Models
+{
+    public class MenuGroup
+    {
+        public string? Title { get; set; }
+        public List<MenuItem> Items { get; set; } = new();
+    }
+}
---
+++ Neurino.Desktop/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Neurino.Desktop
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
